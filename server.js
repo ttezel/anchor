7efa0481adbbe85bc568976b115e57612d0c8e29
Tolga Tezel <tolga-tezel@hotmail.com>
2011-11-01 22:08:56 +0000

    added flow control

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var fs = require('fs')
+var async = require('async')
+  , fs = require('fs')
   , util = require('util')
   , sys = require('sys');
 
@@ -6,61 +7,69 @@ var Rsync = function() {
   this.streamOpts = {
     bufferSize: 500   //S
   };
+  this.blocks = []; //blocks of size bufferSize
   this.weakChkSums = [];
   this.strongChkSums = [];
 };
 
 Rsync.prototype.send = function(filename) {
   var self = this;
-
-  //file is split into blocks of size blockSize
-  this.blocks = [];
   
   //create stream
-  var fStream = fs.createReadStream(filename, this.streamOpts);
+  fs.createReadStream(filename, this.streamOpts)
+    .on('error', function (err) {
+      sys.puts('err', err);
+    })
+    .on('data', function (buff) {
+      self.blocks.push(buff);
+    })
+    .on('end', function () {
+      self.getWeakChkSum();
+    });
   
-  fStream.on('error', function (err) {
-    sys.puts('err', err);
-  });
-  fStream.on('data', function (buff) {
-    self.blocks.push(buff);
-  });
-  fStream.on('end', function () {
-    self.getWeakChkSum();
-  });
   return this;
 };
 
 //calculate weak checksum for each block
 Rsync.prototype.getWeakChkSum = function() {
   var self = this, a = [], b = [];
+
+  async.series([
+      function(cb) {
+        self.blocks.forEach(function(buff, blockNum) {
+          var len = buff.length, lastOffset = len - 1;
+          
+          //calculate block 'a' value
+          a[blockNum] = 0;
+          for(var i = 0; i < len; i++) {  //iterate byte-by-byte
+            a[blockNum] += buff[i];     
+          }
+          
+          //calculate block 'b' value
+          b[blockNum] = 0;
+          for(var i = 0; i < len; i++) {
+            b[blockNum] += (lastOffset - i + 1)*buff[i];      
+          }
+        });
+        
+        cb(null);
+      }
+    , function (cb) {
+        a = a.map(function(sum) { return sum%self.streamOpts.bufferSize });
+        b = b.map(function(sum) { return sum%self.streamOpts.bufferSize });
+        
+        cb(null);
+      }
+    , function() {
+      var numBlocks = self.blocks.length;
   
-  this.blocks.forEach(function(buff, blockNum) {    //calculate 'a' for each block
-    var len = buff.length, lastOffset = len - 1;
-    
-    //calculate block 'a' value
-    a[blockNum] = 0;
-    for(var i = 0; i < len; i++) {  //iterate byte-by-byte
-      a[blockNum] += buff[i];     
+      for(var i = 0; i < numBlocks; i++) {
+        self.weakChkSums[i] = a[i] + b[i]*Math.pow(2,16);   
+      }
+      
+      sys.puts('weakChkSums', self.weakChkSums);
     }
-    
-    //calculate block 'b' value
-    b[blockNum] = 0;
-    for(var i = 0; i < len; i++) {
-      b[blockNum] += (lastOffset - i + 1)*buff[i];      
-    }
-  });
-  //TODO: establish flow control for doing mapping functions last
-  a = a.map(function(sum) { return sum%self.streamOpts.bufferSize });
-  b = b.map(function(sum) { return sum%self.streamOpts.bufferSize });
-  
-  var numBlocks = this.blocks.length;
-  
-  for(var i = 0; i < numBlocks; i++) {
-    this.weakChkSums[i] = a[i] + b[i]*Math.pow(2,16);   
-  }
-  
-  console.log('a', a, 'b', b, 'weakChkSums', this.weakChkSums);
+  ]);
 };
 
 var rsync = new Rsync().send('file.html');
