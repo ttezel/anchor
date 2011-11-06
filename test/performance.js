401550075b6660943baf61ab0d66960bbfe6abc1
Mihai Tomescu <matomesc@gmail.com>
2011-11-06 15:45:24 -0500

    laughable performance

--- test/performance.js
+++ test/performance.js
@@ -5,6 +5,10 @@ var sync = rsync.createRSync('./performance', 750);
 var t1 = Date.now();
 sync.checksum('/libruby190.a', function (err, results) {
     var t2 = Date.now();
-    console.log(results);
     console.log('checksum: %d', t2 - t1);
+    var t3 = Date.now();
+    sync.diff('/libruby191.a', results, function (err, results) {
+        var t4 = Date.now();
+        console.log('diff: %d', t4 - t3);
+    });
 });
