28f5834802f422d52e985c7537782396c6c0060b
Mihai Tomescu <matomesc@gmail.com>
2011-11-06 18:11:49 -0500

    huge performance boost

--- test/performance.js
+++ test/performance.js
@@ -1,14 +1,13 @@
 var rsync = require('../lib/node-rsync.js');
 
-var sync = rsync.createRSync('./performance', 750);
+var sync = rsync.createRSync('./performance', 1000);
 
 var t1 = Date.now();
-sync.checksum('/libruby190.a', function (err, results) {
+sync.checksum('/libruby190.a', function (err, checksums) {
     var t2 = Date.now();
-    console.log('checksum: %d', t2 - t1);
-    var t3 = Date.now();
-    sync.diff('/libruby191.a', results, function (err, results) {
-        var t4 = Date.now();
-        console.log('diff: %d', t4 - t3);
+    sync.diff('/libruby191.a', checksums, function (err, diffs) {
+        var t3 = Date.now();
+        console.log('checksums - time: %d, num: %d', t2 - t1, checksums.length);  
+        console.log('diff - time: %d, num: %d', t3 - t2, diffs.length);
     });
 });
