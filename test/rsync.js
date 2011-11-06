712f399c95b8ed79fb81894c0d728fa83cf34607
Mihai Tomescu <matomesc@gmail.com>
2011-11-06 11:42:59 -0500

    tax;

--- test/rsync.js
+++ test/rsync.js
@@ -3,8 +3,10 @@ var rsync = require('../lib/node-rsync');
 var sync = rsync.createRSync('../lib/files', 2);
 
 sync.checksum('/server.txt', function (err, results) {
+    var t1 = Date.now();
     sync.diff('/client.txt', results, function (err, results) {
+        var t2 = Date.now();
         console.log(results);
+        console.log('total time: %d', t2 - t1);
     });
-    // console.log(results);
 });
