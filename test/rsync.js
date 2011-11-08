4c0f876950d3076498ef8035791c8a1684dca47b
Tolga Tezel <tolgatezel11@gmail.com>
2011-11-08 00:30:42 -0500

    sync started

--- test/rsync.js
+++ test/rsync.js
@@ -4,9 +4,12 @@ var sync = rsync.createRSync('../lib/files', 2);
 
 sync.checksum('/server.txt', function (err, results) {
     var t1 = Date.now();
-    sync.diff('/client.txt', results, function (err, results) {
+    sync.diff('/client.txt', results, function (err, syncdata) {
         var t2 = Date.now();
-        console.log(results);
         console.log('total time: %d', t2 - t1);
+
+        sync.sync('/server.txt', syncdata, function(err, results) {
+
+        });
     });
 });
