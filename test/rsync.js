6b5841b4bbc71ee9a7f8e4417596525374a45078
Tolga Tezel <tolgatezel11@gmail.com>
2011-11-08 20:51:27 -0500

    sync working, error found in diff

--- test/rsync.js
+++ test/rsync.js
@@ -4,12 +4,13 @@ var sync = rsync.createRSync('../lib/files', 2);
 
 sync.checksum('/server.txt', function (err, results) {
     var t1 = Date.now();
-    sync.diff('/client.txt', results, function (err, syncdata) {
+    sync.diff('/client.txt', results, function (err, diff) {
         var t2 = Date.now();
         console.log('total time: %d', t2 - t1);
 
-        sync.sync('/server.txt', syncdata, function(err, results) {
-
+        sync.sync('/server.txt', diff, function(err, results) {
+            if(err) { throw err; }
+            console.log(results.toString());
         });
     });
 });
